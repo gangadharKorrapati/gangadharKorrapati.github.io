16377111d601e1922ea7e4e7d07b6bf93a2d283e
Gitea <gitea@fake.local>
2021-06-21 23:38:21 +0530

    lighthouse

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,9 +15,9 @@ const Hero = () => {
         <img className="max-w-xs" src="img/gangadhar_animated.svg" alt="Gangadhar Image"/>
         <h1 className="pt-4 text-2xl text-gray-700 sm:text-3xl">I am Gangadhar Korrapati</h1>
         <h2 className="py-2 text-xl text-gray-500 sm:text-2xl">A Systems Engineer at TCS</h2>
-        <span className="px-4 py-2 text-lg text-white cursor-pointer sm:text-xl max-w-min rounded-2xl bg-gradient-to-r from-green-400 to-blue-400 hover:from-red-400 hover:to-yellow-500 whitespace-nowrap" onClick={(e) => handleClick(e, "/about")}>
-            Tell me More
-        </span>
+        <button className="flex px-4 py-2 text-lg text-white cursor-pointer sm:text-xl max-w-min rounded-2xl bg-gradient-to-r from-green-400 to-blue-400 hover:from-red-400 hover:to-yellow-500 whitespace-nowrap focus:outline-none focus:ring" onClick={(e) => handleClick(e, "/about")} aria-label="Tell Me More">
+            Tell Me More
+        </button>
     </div>
     )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ const index = () => {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Gangadhar Korrapati</title>
         <link rel="icon" href="/favicon.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
