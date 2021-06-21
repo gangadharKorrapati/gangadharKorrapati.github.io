1ca00c394b1ba262882b9c8f192429ff086e5e23
gangadharKorrapati <g4g7g9@gmail.com>
2021-06-21 18:09:17 +0000

    Deploying to gh-pages from @ gangadharKorrapati/gangadharKorrapati.github.io@16377111d601e1922ea7e4e7d07b6bf93a2d283e 🚀

--- _next/static/JdKOoCQXd6fvgsVwWdODw/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-47045def556eb5e5fd86.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],"/about":["static/chunks/pages/about-26e313b27bef45d25083.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
