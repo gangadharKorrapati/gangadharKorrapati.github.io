c95c7f726ed2b0af0d53b8b19d1da75b578aa6d4
GANGADHAR KORRAPATI <g4g7g9@gmail.com>
2022-03-18 06:19:04 -0400

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
 	<div className="flex flex-col items-center pb-4 font-mono">
         <img className="max-w-xs" src="img/hero.svg" alt="My Image"/>
         <h1 className="pt-4 text-2xl text-gray-700 sm:text-3xl">I am Gangadhar Korrapati</h1>
-        <h2 className="py-2 text-xl text-gray-500 sm:text-2xl">A CS grad From SASTRA University</h2>
+        <h2 className="py-2 text-xl text-gray-500 sm:text-2xl">A Graduate Student in Computer Science at SUNY Binghamton</h2>
         <button className="flex px-4 py-2 text-lg text-white cursor-pointer sm:text-xl max-w-min rounded-2xl bg-gradient-to-r from-green-400 to-blue-400 hover:from-red-400 hover:to-yellow-500 whitespace-nowrap focus:outline-none focus:ring" onClick={(e) => handleClick(e, "/about")} aria-label="Tell Me More">
             Tell Me More
         </button>
